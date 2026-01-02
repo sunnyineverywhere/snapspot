d014e830aa5f1e28d26831c360dbae1b392c6589
sunnyineverywhere <sunnyinsummer@ewhain.net>
2026-01-02 17:06:34 +0900

    refactor: 프로젝트 삭제

--- backend/src/main/java/snap/infrastructure/batch/config/ComponentScanConfiguration.java
+++
@@ -1,8 +0,0 @@
-package snap.infrastructure.batch.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"snap"})
-class ComponentScanConfiguration {}
