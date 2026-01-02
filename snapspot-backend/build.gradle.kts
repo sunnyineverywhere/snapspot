e1c2c242506327ba5d1d0b42bea077da9bfb0271
sunnyineverywhere <sunnyinsummer@ewhain.net>
2026-01-02 19:12:16 +0900

    chore(#6): 패키지 오타 수정 및 jackson에서 제공하는 kotlin 모듈을 공식버전으로 변경
    - 스프링 이니셜라이저 그대로 사용했더니 ide 사용에 문제가 발생함(스프링부트 인식을 못함)
    - 패키지 변경 및 정리

--- snapspot-backend/build.gradle.kts
+++ snapspot-backend/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 	kotlin("plugin.jpa") version "2.2.21"
 }
 
-group = "xyz.snapsnpot"
+group = "xyz.snapspot"
 version = "0.0.1-SNAPSHOT"
-description = "backend system for snapspot"
+description = "backend system for Snapspot"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+		languageVersion = JavaLanguageVersion.of(17)
 	}
 }
 
@@ -22,15 +22,13 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("tools.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	implementation("org.springframework.boot:spring-boot-h2console")	
 	runtimeOnly("com.h2database:h2")
 	
-	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
--- snapspot-backend/settings.gradle.kts
+++ snapspot-backend/settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "snapspot-backend"
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
