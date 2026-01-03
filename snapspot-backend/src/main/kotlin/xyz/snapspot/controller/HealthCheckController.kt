ca5d334566f4edf2d42e0ac3c44bcfab3ab23e32
sunnyineverywhere <sunnyinsummer@ewhain.net>
2026-01-03 17:39:25 +0900

    chore(#6): 불필요한 파일 삭제

--- snapspot-backend/src/main/kotlin/xyz/snapspot/controller/HealthCheckController.kt
+++ snapspot-backend/src/main/kotlin/xyz/snapspot/controller/HealthCheckController.kt
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/health-check")
 class HealthCheckController {
-    // convert String to ResponseEntity with status 200 OK
     @GetMapping
     fun healthCheck(): ResponseEntity<String> = ResponseEntity.ok("OK")
 }
